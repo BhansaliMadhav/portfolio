a793fc59ad529bf685eee683e4633f12d5016d94
= <=>
2025-06-17 23:00:48 +0530

    fixed ui isues in deployment

--- app/utils/theme-provider.tsx
+++ app/utils/theme-provider.tsx
@@ -17,17 +17,7 @@ export function ThemeProvider({
   specifiedTheme: Theme | null;
 }) {
   const [theme, setTheme] = useState<Theme>(() => {
-    if (specifiedTheme) {
-      return specifiedTheme;
-    }
-
-    if (typeof window !== "undefined") {
-      return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
-    }
-
-    return "dark";
+    return "light";
   });
 
   useEffect(() => {
